d2035f8e991b4f8800bd9bc87b9023a2247dba0a
bhimraj12 <66239418+bhimraj12@users.noreply.github.com>
2023-11-12 08:39:03 +0530

    dtabase

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '3.88.42.7',
 	'username' => 'root',
 	'password' => 'password',
 	'database' => 'eshop_vendor',
