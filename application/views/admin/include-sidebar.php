cf54b141d7a782e74ce95a324720f72d9a8130be
RahulTamatta <69134531+RahulTamatta@users.noreply.github.com>
2023-12-10 07:11:17 -0800

    removed cod

--- application/views/admin/include-sidebar.php
+++ application/views/admin/include-sidebar.php
@@ -242,14 +242,7 @@ $doctor_brown_for_app = get_settings('doctor_brown'); ?>
                                 </li>
                             <?php } ?>
 
-                            <?php if (has_permissions('read', 'cod_delivery_charges')) { ?>
-                                <li class="nav-item">
-                                    <a href="<?= base_url('admin/cod/charges') ?>" class="nav-link">
-                                        <i class="icon-basket"></i>
-                                        <p>Cod Delivery Charge</p>
-                                    </a>
-                                </li>
-                            <?php } ?>
+                       
 
        
                         </ul>
