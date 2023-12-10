77af35e8625382f73410e7336c9d54a27dad2158
bhimraj12 <66239418+bhimraj12@users.noreply.github.com>
2023-12-10 19:16:29 +0530

    logs removed

--- application/logs/log-2023-11-12.php
+++
@@ -1,6 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2023-11-12 04:07:46 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond C:\xampp\htdocs\Ecart\system\database\drivers\mysqli\mysqli_driver.php 211
-ERROR - 2023-11-12 04:07:46 --> Unable to connect to the database
-ERROR - 2023-11-12 04:07:57 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond C:\xampp\htdocs\Ecart\system\database\drivers\mysqli\mysqli_driver.php 211
-ERROR - 2023-11-12 04:07:57 --> Unable to connect to the database
